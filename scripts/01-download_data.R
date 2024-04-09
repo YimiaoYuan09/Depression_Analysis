90c426947347b6f8520d49c567ce6b1cd6e52a71
Yimiao Yuan <yymlinda.yuan@mail.utoronto.ca>
2024-04-08 22:21:42 -0400

    download raw data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,26 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the raw data from Mendeley Data on the
+# effects of pets on depression
+# Author: Yimiao Yuan
+# Date: 08 April 2024
+# Contact: yymlinda.yuan@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: --
+# The raw dataset is available at: https://data.mendeley.com/datasets/bvrzf2s9j7/6
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(haven)
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
+# the raw data is downloaded from the website and
+# saved in the /data/raw_data folder
+raw_pet_data <- read_sav("data/raw_data/raw_data.sav")
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(raw_pet_data, "data/raw_data/raw_pet_data.csv")
 
          
